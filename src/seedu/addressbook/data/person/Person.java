427ac2fa76fdc9f0b206ad638ffcd79cc02f94c9
josephinetanadi <josephinetanadi@gmail.com>
2019-02-05 15:56:40 +0800

    Implement class-level members

--- src/seedu/addressbook/data/person/Person.java
+++ src/seedu/addressbook/data/person/Person.java
@@ -17,6 +17,9 @@ public class Person implements ReadOnlyPerson {
     private Email email;
     private Address address;
 
+    private int sequenceNumber;
+    private static int nextSequenceNumber = 1;
+
     private final Set<Tag> tags = new HashSet<>();
 
     /**
@@ -28,6 +31,9 @@ public class Person implements ReadOnlyPerson {
         this.email = email;
         this.address = address;
         this.tags.addAll(tags);
+
+        sequenceNumber = nextSequenceNumber;
+        nextSequenceNumber++;
     }
 
     /**
